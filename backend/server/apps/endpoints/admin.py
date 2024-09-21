7ee013ab6727c22dce746eea56859554759b0e76
Mir Ilham <mhmilham3@gmail.com>
2024-09-22 02:32:53 +0600

    Register models to admin site

--- backend/server/apps/endpoints/admin.py
+++ backend/server/apps/endpoints/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from apps.endpoints.models import Endpoint
+from apps.endpoints.models import MLAlgorithm
+from apps.endpoints.models import MLAlgorithmStatus
+from apps.endpoints.models import MLRequest
+
 # Register your models here.
+admin.site.register(Endpoint)
+admin.site.register(MLAlgorithm)
+admin.site.register(MLAlgorithmStatus)
+admin.site.register(MLRequest)
